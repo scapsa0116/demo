484da3bd5b4c3b75d4179ea6e9359c872f7142b5
stela <stela.capsa@gmail.com>
2021-04-14 13:44:47 -0500

    demo for an intership

---
+++ src/components/TextComponent.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+
+class TextComponent extends React.Component{
+
+
+
+
+
+
+    render(){
+        return(
+            <div>text</div>
+        )
+    }
+}
+
+
+export default TextComponent
